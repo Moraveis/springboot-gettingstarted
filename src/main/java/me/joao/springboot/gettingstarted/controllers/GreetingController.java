7e64df25be4c185a74d02436076570259435e60c
Joao Vitor <joaovitormoraveis@gmail.com>
2023-02-07 21:09:06 -0500

    adding basic replace for custom properties

--- src/main/java/me/joao/springboot/gettingstarted/controllers/GreetingController.java
+++ src/main/java/me/joao/springboot/gettingstarted/controllers/GreetingController.java
@@ -1,5 +1,6 @@
 package me.joao.springboot.gettingstarted.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +9,19 @@ import java.time.LocalDateTime;
 @RestController
 public class GreetingController {
 
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
     @GetMapping("/")
     public String greeting() {
         return "Greetings! It is " + LocalDateTime.now();
+    }
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo(){
+        return "Coach name = " + coachName + " - team name = " + teamName;
     }
 }
