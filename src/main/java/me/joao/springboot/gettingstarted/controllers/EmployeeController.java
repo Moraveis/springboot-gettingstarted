c8532010ff6ef5acc124fc3f733e878b76ec43a1
Joao Vitor <joaovitormoraveis@gmail.com>
2023-02-09 19:23:15 -0500

    refactoring to use service

--- src/main/java/me/joao/springboot/gettingstarted/controllers/EmployeeController.java
+++ src/main/java/me/joao/springboot/gettingstarted/controllers/EmployeeController.java
@@ -2,8 +2,13 @@ package me.joao.springboot.gettingstarted.controllers;
 
 import me.joao.springboot.gettingstarted.dao.EmployeeDAO;
 import me.joao.springboot.gettingstarted.entity.Employee;
+import me.joao.springboot.gettingstarted.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -11,15 +16,30 @@ import java.util.List;
 @RestController
 public class EmployeeController {
 
-    private final EmployeeDAO employeeDAO;
+    private final EmployeeService employeeService;
 
     @Autowired
-    public EmployeeController(EmployeeDAO employeeDAO) {
-        this.employeeDAO = employeeDAO;
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
     }
 
     @GetMapping("/employees")
     public List<Employee> findAll() {
-        return employeeDAO.findAll();
+        return employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee findById(@PathVariable("id") Integer id) {
+        return employeeService.findById(id);
+    }
+
+    @PostMapping("/employee")
+    public void createEmployee(@RequestBody Employee employee) {
+        employeeService.save(employee);
+    }
+
+    @DeleteMapping("/employee/{id}")
+    public void deleteEmployee(@PathVariable("id") Integer id) {
+        employeeService.deleteById(id);
     }
 }
