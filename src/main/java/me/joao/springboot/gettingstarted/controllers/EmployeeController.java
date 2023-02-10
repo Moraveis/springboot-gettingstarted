1b8cbca668929e25629365f60401f62b712946f4
Joao Vitor <joaovitormoraveis@gmail.com>
2023-02-09 19:35:20 -0500

    refactoring to use service

--- src/main/java/me/joao/springboot/gettingstarted/controllers/EmployeeController.java
+++ src/main/java/me/joao/springboot/gettingstarted/controllers/EmployeeController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,6 +36,12 @@ public class EmployeeController {
 
     @PostMapping("/employee")
     public Employee createEmployee(@RequestBody Employee employee) {
+        employeeService.save(employee);
+        return employee;
+    }
+
+    @PutMapping("/employee")
+    public Employee updateEmployee(@RequestBody Employee employee) {
         employeeService.save(employee);
         return employee;
     }
